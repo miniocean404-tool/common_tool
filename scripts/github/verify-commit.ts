b81abcd2dcebfff006c92bd773885f8be770eb8d
MiniOcean404 <1037306928@qq.com>
2024-11-21 02:06:16 +0800

    ✨ feat: 代码校验

--- scripts/github/verify-commit.ts
+++ scripts/github/verify-commit.ts
@@ -1,9 +1,10 @@
-// Invoked on the commit-msg git hook by yorkie.
+import fs from "fs"
+import pc from "picocolors"
+import dedent from "dedent"
 
-const chalk = require("chalk")
-// Yorkie的gitHooks使用process.env.GIT_PARAMS
+// Yorkie 的 gitHooks 使用 process.env.GIT_PARAMS
 const msgPath = process.argv[2]
-const msg = require("fs").readFileSync(msgPath, "utf-8").trim()
+const msg = fs.readFileSync(msgPath, "utf-8").trim()
 
 // revert:回滚到上一个版本
 
@@ -27,15 +28,20 @@ const mergeRe = /^(Merge pull request|Merge branch)/
 
 if (!commitRE.test(msg)) {
   if (!mergeRe.test(msg)) {
-    console.log("提交的消息: ", msg)
-    console.error(
-      ` ${chalk.bgRed.white(" 错误 ")} ${chalk.red(`错误的消息格式`)}\n\n${chalk.red(
-        `  自动化校验-提交模板样例:\n\n`,
-      )}    ${chalk.green(`feat(compiler): add 'comments' option（冒号后有空格）`)}\n` +
-        `    ${chalk.green(`fix(v-model): handle events on blur (close #28)`)}\n\n${chalk.red(
-          `  查看文档详情：https://github.com/vuejs/vue-next/blob/master/.github/commit-convention.md for more details.\n`,
-        )}`,
-    )
+    console.log(`提交的消息: ${msg}`)
+
+    const template = `
+          ${pc.bgRed("错误:")}
+            ${pc.yellow(`自动化校验-提交模板样例:`)}
+
+              ${pc.green(`feat(compiler): add 'comments' option（冒号后有空格）`)}
+              ${pc.green(`fix(v-model): handle events on blur (close #28)`)}
+
+            ${pc.yellow(`查看文档详情：https://github.com/vuejs/vue-next/blob/master/.github/commit-convention.md for more details.`)}
+    `
+
+    console.log(dedent(template))
+
     process.exit(1)
   }
 }
